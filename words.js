40c8aac3fea0bf722853f54f4f6ec924cecf3abc
jiaqian.shi <jiaqian.shi@intous.com>
2023-04-10 10:24:49 +0800

    commit

---
+++ words.js
@@ -0,0 +1,38 @@
+const words = [
+    {
+        word: 'That song is about courage and going into the unknown.',
+        translation: '那首歌讲的是勇敢和对未知的探索'
+    },
+    {
+        word: "Beautiful things don't ask for attention.",
+        translation: '美好的东西从来不会寻求关注'
+    },
+    {
+        word: "I don't like to have the distraction of the camera.",
+        translation: '我不喜欢为了拍照而拍照'
+    },
+    {
+        word: "Just want to stay in it.",
+        translation: '只想享受在那一刻'
+    },
+    {
+        word: 'To see the world, things dangerous to come to',
+        translation: '认识世界，克服困难',
+    },
+    {
+        word: 'To see behind walls',
+        translation: '洞悉所有'
+    },
+    {
+        word: 'To draw closer',
+        translation: '贴近生活'
+    },
+    {
+        word: 'To find each other and to feel',
+        translation: '寻找真爱，感受彼此'
+    },
+    {
+        word: 'That is the purpose of LIFE',
+        translation: '这就是人生的目的'
+    }
+];
